44efd7bcaf9d41a850607dd4eb482b786c072fbe
Abdul Gafoor <abdul.gafoor@Eternus.local>
2019-01-23 19:01:13 +0530

    commit message

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 var app=express();
 
-app.set('view engine', 'ejs').get('/',(req,res)=>{
-//res.send('in index js node');
-res.render('pages/index');
+app.get('/',(req,res)=>{
+res.send('in index js node');
 });
 
 app.listen(PORT,()=>console.log('listening on port'));
