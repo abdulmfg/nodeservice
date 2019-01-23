3d5e17efa00fee6e86252cac99b19681c8bdabba
Abdul Gafoor <abdul.gafoor@Eternus.local>
2019-01-23 18:26:30 +0530

    commit message

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 var app=express();
 
-app.use(express.static(path.join(__dirname,'public')));
-app.set('views',path.join(__dirname,'views'))
-app.set('view engine', 'ejs');
-app.get('/baja',(req,res)=>{
+app.use(express.static(path.join(__dirname,'public'))).set('views',path.join(__dirname,'views')).set('view engine', 'ejs').get('/',(req,res)=>{
 //res.send('in index js node');
 res.render('/pages/index');
 });
