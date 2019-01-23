658836e379e499ecff4dc1ffda852d8fcd42e569
Abdul Gafoor <abdul.gafoor@Eternus.local>
2019-01-23 12:21:43 +0530

    change in server.js console log

--- server.js
+++ server.js
@@ -1,11 +1,1 @@
-var express = require('express');
-var app = express();
-
-
-app.get('/',function(request,response){
-   response.send('From Server'); 
-});
-
-app.listen(3000,function(){
-    console.log('Server running on port 3000');
-});
+console.log('hello hroku');
