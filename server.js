928705cc15a3c582181d3d501ec8152523466aaf
Abdul Gafoor <abdul.gafoor@Eternus.local>
2019-01-23 18:28:21 +0530

    commit message

--- server.js
+++ server.js
@@ -3,12 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 var app=express();
 
-app.use(express.static(path.join(__dirname,'public'))).set('views',path.join(__dirname,'views')).set('view engine', 'ejs').get('/',(req,res)=>{
-//res.send('in index js node');
-res.render('/pages/index');
-});
-
-app.listen(PORT,()=>console.log('listening on port'));
+app.use(express.static(path.join(__dirname,'public'))).set('views',path.join(__dirname,'views')).set('view engine', 'ejs').get('/',(req,res)=>res.render('/pages/index')).listen(PORT,()=>console.log('listening on port'))
 
 
 
