eb61dfc4591be11dedd6509f1dd7042c8eb6f10a
Abdul Gafoor <abdul.gafoor@Eternus.local>
2019-01-24 12:47:02 +0530

    commit message

--- server.js
+++ server.js
@@ -1,13 +1,41 @@
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+let bodyParser = require('body-parser');
 var app=express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended :false}))
+let employees=[
+    {
+        "id":1,
+        "name":"abdul"
+    },
+    {
+        "id":2,
+        "name":"shivani"
+    },
+    {
+        "id":3,
+        "name":"aman"
+    }
+];
 app.get('/',(req,res)=>{
-res.send('in index js node');
+let tempEmployee = req.headers;
+  //  console.log(tempEmployee)
+if(tempEmployee)
+employees.push(req.body);
+res.send(employees);
 });
 
-app.listen(PORT,()=>console.log('listening on port'));
+app.post('/',(req,res)=>{
+    let emp = req.body;
+    console.log(emp)
+    if(!emp || emp.name===""){
+        res.send({"error":"Name can't be empty"});
+    }else{
+        employees.push(emp);
+        res.send(employees);
+    }
+})
+app.listen(3000,()=>console.log('listening on port'));
 
 
 
